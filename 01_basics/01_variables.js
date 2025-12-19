8010a988b3982e0bb71ad1591442b3c9725c1a85
rishi-trehan <46898836+rishi-trehan@users.noreply.github.com>
2025-12-19 19:21:58 +0000

    datatypes and comparision

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 14455
+let accountEmail = "abc@gmail.com"
+var accountPassword = "11111"
+accountCity="Hoshiarpur"
+let accountState;
+//accountId = 2    // Not allowed
+
+accountEmail = "xyz@gmail.com"
+accountPassword = "22222"
+accountCity="Noida"
+
+console.log(accountId);
+console.table([accountId, accountEmail , accountPassword, accountCity, accountState]);
+
